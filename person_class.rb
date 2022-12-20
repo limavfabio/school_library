e078f8a7bbddc135a755d906e72c6cdd2efde62a
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-12-20 12:36:30 -0300

    Add person Class

---
+++ person_class.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Person Class
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1...1000)
+    @parent_permission = parent_permission
+    @name = name
+    @age = age
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+end
