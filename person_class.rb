3cc9bf8d256ef5a34371b5ae9c8e24c8af34fee8
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-12-20 12:52:19 -0300

    Person inherits nameable

--- person_class.rb
+++ person_class.rb
@@ -1,10 +1,12 @@
-# Person Class
-class Person
+require_relative './nameable/nameable_decorator'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(1...1000)
+    super()
+    @id = Random.rand(1..1000)
     @parent_permission = parent_permission
     @name = name
     @age = age
@@ -20,5 +22,9 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
