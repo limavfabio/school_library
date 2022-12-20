6a3664c3698a5592160308055bb421184ec93dd5
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-12-20 12:36:44 -0300

    Add teacher class

---
+++ teacher_class.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative 'person_class'
+
+# Teacher Class
+class Teacher < Person
+  attr_reader :specialization
+
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    @specialization = specialization
+    super(age, name, parent_permission: parent_permission)
+  end
+
+  def can_use_services?
+    true
+  end
+end
