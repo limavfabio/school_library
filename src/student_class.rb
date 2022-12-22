41f5981539f41f35c342414c5e94da6d396f1d06
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-12-22 20:35:39 -0300

    Make classroom argument optional

--- src/student_class.rb
+++ src/student_class.rb
@@ -3,10 +3,10 @@ require_relative 'person_class'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', classroom = 'Unknown')
+  def initialize(age, name = 'Unknown', classroom = 'Unknown', parent_permission = true)
     super(age, name)
     @classroom = classroom
-    classroom.add_student(self)
+    classroom&.add_student(self)
   end
 
   def play_hooky
