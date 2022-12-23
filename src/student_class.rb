407ba80cac769009499aaaaa42d9d843fc339c8a
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-12-23 09:10:18 -0300

    Fix student constructor parameters

--- src/student_class.rb
+++ src/student_class.rb
@@ -3,9 +3,10 @@ require_relative 'person_class'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', classroom = 'Unknown', _parent_permission: true)
+  def initialize(age, name, classroom, parent_permission)
     super(age, name)
     @classroom = classroom
+    @parent_permission = parent_permission
     classroom&.add_student(self)
   end
 
