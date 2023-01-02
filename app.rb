da35cdec98fdd4343440b68b51bce3ef04186189
Camilovelag <camilo.vela.galindo@gmail.com>
2023-01-02 17:19:09 -0500

    decouple create_rental method

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require './src/classroom_class'
 require './src/book_class'
 require './src/nameable/capitalize_decorator'
 require './src/nameable/trimmer_decorator'
+require './src/manage_rentals'
 
 class App
   def initialize
@@ -68,26 +69,7 @@ class App
   end
 
   def create_rental
-    puts 'Choose a book from the following list by number'
-    @books_list.each_with_index do |book, index|
-      puts "#{index + 1}) Title: #{book.title}, Author: #{book.author}"
-    end
-    print 'Book number: '
-    book_number = gets.chomp.to_i - 1
-    puts 'Choose a person from the following list by number'
-    @people_list.each_with_index do |person, index|
-      puts "#{index + 1}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-    print 'Person number: '
-    person_number = gets.chomp.to_i - 1
-    print 'Date: '
-    date = gets.chomp
-    book = @books_list.at(book_number)
-    person = @people_list.at(person_number)
-    book.add_rental(person, date)
-    rental = Rental.new(person, book, date)
-    @rentals_list << rental
-    puts 'Rental created successfully'
+    ManageRentals.new.create_rental(@books_list, @people_list, @rentals_list)
   end
 
   def list_rentals_for_person_id
