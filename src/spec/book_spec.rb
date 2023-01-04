550571c4ee233800f7125e0808005263434071f5
Fábio Vinícius Lima <limavfabio@gmail.com>
2023-01-04 17:41:44 -0300

    Add book_class spec

---
+++ src/spec/book_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require_relative '../book_class'
+
+describe Book do
+  let(:book) { Book.new('War and Peace', 'Leo Tolstoy') }
+
+  describe '#initialize' do
+    it 'sets the title and author' do
+      expect(book.title).to eq('War and Peace')
+      expect(book.author).to eq('Leo Tolstoy')
+    end
+  end
+end
