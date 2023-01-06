98bd04f0ed2582ba7a3c5a74e586b2eae272a058
Fábio Vinícius Lima <limavfabio@gmail.com>
2023-01-06 12:56:22 -0300

    Add factorial method

--- src/solver_class.rb
+++ src/solver_class.rb
@@ -1,3 +1,11 @@
 class Solver
-
+  def factorial(n)
+    if n < 0
+      raise 'Factorial is defined only for non-negative integers'
+    elsif n == 0
+      1
+    else
+      (1..n).inject(:*)
+    end
+  end
 end
